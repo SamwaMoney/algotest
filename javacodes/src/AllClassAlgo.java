2541923ac8521801a2ae4cdc976185dbbf78d7d8
hanby-choi <hanby.choi@ewhain.net>
2023-07-17 20:58:00 +0900

    feat: 전체 데이터 관련 코멘트 추가 알고리즘 보완 및 점수 로직 수정

--- javacodes/src/AllClassAlgo.java
+++ javacodes/src/AllClassAlgo.java
@@ -7,18 +7,15 @@ public class AllClassAlgo {
 
     // 전체 과목 개수 count
     // 수업 7개 미만: 수업 하나당 +1, 원격 비대면일 경우 +7
-    public static int AllClassesAlgo(Table t) {
+    public static int allClassAlgo(Table t, ArrayList<Integer> good, ArrayList<Integer> bad, ArrayList<Integer> special) {
         Set<String> allClasses = new HashSet<>(); // 모든 강의 리스트
 
         int allCount = 0; // 모든 강의의 수 저장
         int online = 0; // 원격/온라인 강의의 수 저장
         int score = 0; // 점수 저장
-        int allBlockCount = t.classList.size(); // 전체 수업 단위 수 저장
+        int allBlockCount = t.classList.size(); // 전체 수업 단위 수 저장 - 연강인 블럭 입력 받았을 때 처리가 필요함
         int chapelCount = ChapelAlgo(t); // 전체 채플 수
-
-        ArrayList<Integer> good = new ArrayList<>();
-        ArrayList<Integer> bad = new ArrayList<>();
-        ArrayList<Integer> special = new ArrayList<>();
+        int locationCount = locationAlgo(t, special); // 전체 강의실 건물 수 
 
         for (Class c : t.classList) {
             if (c.location.equals("원격/비대면")) {
@@ -45,8 +42,17 @@ public class AllClassAlgo {
         if (chapelCount >= 2)
             special.add(3); // special comment id 3 하나님의 축복이 끝이 없네
 
-        DayoffAlgo(t, good, bad); // 공강 관련 가감점 및 코멘트 처리 함수 
-        
+        score += DayoffAlgo(t, good, bad); // 공강 관련 가감점 및 코멘트 처리 함수 
+        score += FirstClassAlgo(t, good, bad); // 1교시 관련 감점 및 코멘트 처리 함수 
+
+        if (allAfternoonAlgo(t))
+            special.add(4); // special comment id 4 상여자 특) 점심 먹고 등교함
+
+        if (locationCount == 1)
+            good.add(11); // good comment id 11 나는 한 건물만 판다
+        else if (locationCount >= 4)
+            special.add(8); // special comment id 8 동에 번쩍 서에 번쩍
+
         System.out.println("과목의 개수가 " + allCount + "개이고, 원격/온라인 강의의 개수는 " + online +"개 이므로, 점수는 " + score);
 
         return score;
@@ -54,7 +60,7 @@ public class AllClassAlgo {
 
     // 공강 개수 count
     // 요일 공강 하나당 +10
-    public static void DayoffAlgo(Table t, ArrayList<Integer> good, ArrayList<Integer> bad) {
+    public static int DayoffAlgo(Table t, ArrayList<Integer> good, ArrayList<Integer> bad) {
 
         boolean[] dayOffs = new boolean[5]; // 공강 여부 저장하는 배열
         int score = 0; // 점수 저장
@@ -91,7 +97,7 @@ public class AllClassAlgo {
                 dayOffCnt++;
         }
 
-        score += dayOffCnt * 10;
+        score = dayOffCnt * 10;
 
         if (dayOffCnt == 1){
             for (int i=0; i<5; i++){
@@ -110,7 +116,7 @@ public class AllClassAlgo {
 
         System.out.println("요일 공강의 개수가 " + dayOffCnt + "개 이므로, 점수는 " + score);
 
-        return;
+        return score;
     }
 
     // 채플 개수 count
@@ -161,7 +167,7 @@ public class AllClassAlgo {
 
     // 1교시 수업 개수 count
     // 1교시 수업 유무: 1교시 수업 하나당 -3
-    public static int FirstClassAlgo(Table t) {
+    public static int FirstClassAlgo(Table t, ArrayList<Integer> good, ArrayList<Integer> bad) {
         int firstClass = 0; // 1교시 수업 개수 저장
         int score = 0; // 점수 저장
 
@@ -172,108 +178,68 @@ public class AllClassAlgo {
             }
         }
 
+        if (firstClass == 0)
+            good.add(8); // good comment id 8 1교시 제로
+        else if (firstClass >= 2)
+            bad.add(1); // bad comment id 1 오전부터 바빠요!
+        
         System.out.println("1교시의 개수가 " + firstClass + "개 이므로, 점수는 " + score);
 
         return score;
     }
 
-    // 강의 장소 모두 같은지 판별
-    public static boolean sameLocationAlgo(Table t) {
+    // 강의 장소 수 세는 알고리즘
+    public static int locationAlgo(Table t, ArrayList<Integer> special) {
 
         Set<String> locations = new HashSet<>(); // 강의 장소 저장
         int locationNum = 0; // 강의 장소의 수 저장
 
         for (Class c : t.classList) {
-            locations.add(c.location);
+            if (c.className.equals("채플")) // 채플은 제외 
+                continue; 
+            else
+                locations.add(c.location);
         }
 
         locationNum = locations.size();
 
-        if (locationNum == 1) {
-            System.out.println(true);
-            return true;
-        } else {
-            System.out.println(false);
-            return false;
+        if (locationNum == 1){
+            Iterator<String> itr = locations.iterator();
+            switch (itr.next()) {
+                case value:
+                    
+                    break;
+            }
         }
 
-    }
-
-    // 강의 장소 모두 다른지 판별
-    public static boolean differentLocationAlgo(Table t) {
-        Set<String> locations = new HashSet<>(); // 모든 강의 장소 저장
-        Set<String> allClasses = new HashSet<>(); // 모든 강의명 저장
-
-        boolean isDifferent;
-
-        for (Class c : t.classList) {
-            locations.add(c.location);
-            allClasses.add(c.className);
-        }
-
-        if (allClasses.size() == 1) {
-            isDifferent = false; // 과목 하나만 있을 때는 수업 장소가 같은 것으로 가정
-        }
-        else if (locations.size() == allClasses.size()) {
-            isDifferent = true;
-        } else {
-            isDifferent = false;
-        }
-
-        System.out.println(isDifferent);
-
-        return isDifferent;
+        return locationNum;
     }
 
     // 강의가 모두 오전인지 판별
     public static boolean allMorningAlgo(Table t) {
-        Set<String> morningClass = new HashSet<>(); // 오전 강의 저장
-
-        boolean isAllMorning;
-
         for (Class c : t.classList) {
-
-            if (c.startH <= 9) {
-                morningClass.add("morning");
+            if (c.endH < 12) {
+                continue;
             } else {
-                morningClass.add("afternoon");
+                System.out.println(false);
+                return false;
             }
         }
-
-        if (morningClass.size() == 1) {
-            isAllMorning = true;
-        } else {
-            isAllMorning = false;
-        }
-
-        System.out.println(isAllMorning);
-
-        return isAllMorning;
+        System.out.println(true);
+        return true;
     }
 
     // 강의가 모두 오후인지 판별
     public static boolean allAfternoonAlgo(Table t) {
-        Set<String> afternoonClass = new HashSet<>(); // 오후 강의 저장
-
-        boolean isAllAfternoon;
-
         for (Class c : t.classList) {
-
-            if (c.startH > 9) {
-                afternoonClass.add("afternoon");
+            if (c.startH >= 12) {
+                continue;
             } else {
-                afternoonClass.add("morning");
+                System.out.println(false);
+                return false;
             }
         }
-
-        if (afternoonClass.size() == 1) {
-            isAllAfternoon = true;
-        } else {
-            isAllAfternoon = false;
-        }
-
-        System.out.println(isAllAfternoon);
-
-        return isAllAfternoon;
+        System.out.println(true);
+        return true;
     }
 }
